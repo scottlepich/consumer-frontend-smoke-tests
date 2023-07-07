d714233d82e4ca496ca7a4a34dadd3612d9dfae8
Scott Lepich <slepich@opentable.com>
2023-07-06 17:32:51 -0700

    update lang/locale test

--- cypress/e2e/opentable.cy.ts
+++ cypress/e2e/opentable.cy.ts
@@ -2,8 +2,9 @@ import locales, { Locale } from "../fixtures/locales";
 
 describe("OpenTable", () => {
   it("should have a main-content container for all supported locales", () => {
-    locales.forEach((locale: Locale) => {
-      cy.visit(`/?lang=${locale}`);
+    locales.forEach((lang: Locale) => {
+      const qs = { lang };
+      cy.visit("/", { qs });
       cy.get("main-content").should("exist");
     });
   });
